d4e86ae6698534846888e471a41b2463d2f7abad
tanisha114 <157787809+tanisha114@users.noreply.github.com>
2024-01-28 12:21:42 +0530

    New Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Hey, Tanisha Tomar .
+        Hey, Tanisha Tomar .You are a girl.
       </p>
     </>
   )
